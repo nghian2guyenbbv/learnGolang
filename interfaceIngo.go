14de22bf7ace8138707735d3f2f6d21d0a38de66
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-05-07 15:28:11 +0700

    update Interface ingo

--- interfaceIngo.go
+++ interfaceIngo.go
@@ -16,6 +16,17 @@ type Contract struct {
 	basicpay int
 }
 
+type FreeLance struct{
+	empId int
+	basicpay int
+	bonusPerfoment int
+
+}
+
+func (f FreeLance) CalculateSalary() int{
+	return f.basicpay + f.bonusPerfoment
+}
+
 func (p Permanent) CalculateSalary() int {
 	return p.basicpay + p.pf
 }	
@@ -26,16 +37,17 @@ func (c Contract) CalculateSalary() int {
 
 func totalExpense(s []SalaryCalculator){
 	expense := 0
-	for _, v :range s{
+	for _, v :=range s{
 		expense = expense + v.CalculateSalary()
 	}
-	fmt.Printf("total expense is: , %d", expense)
+	fmt.Printf("total expense is: %d", expense)
 }
 
 func main(){
-	pemp1 = Permanent{1,200,300}
-	pemp2 = Permanent{2,300, 500}
-	cemp1 = Contract{3, 500}
-	employess := []SalaryCalculator{pemp1, pemp2, cemp1}
+	pemp1 := Permanent{1,200,300}
+	pemp2 := Permanent{2,300, 500}
+	cemp1 := Contract{3, 500}
+	femp1 := FreeLance{4, 600, 400}
+	employess := []SalaryCalculator{pemp1, pemp2, cemp1, femp1}
 	totalExpense(employess)
 }
